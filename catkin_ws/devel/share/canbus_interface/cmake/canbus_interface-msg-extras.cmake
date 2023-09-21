0b26879c85391a3fa1bb62b5b12d826a844f77bf
Daniel Frau Alfaro <98766327+DanielFrauAlfaro@users.noreply.github.com>
2023-09-21 16:55:06 +0200

    Delete catkin_ws/devel directory

--- catkin_ws/devel/share/canbus_interface/cmake/canbus_interface-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(canbus_interface_MESSAGE_FILES "/daniel/Desktop/evo_pipe/catkin_ws/src/canbus_interface/msg/CanFrame.msg")
-set(canbus_interface_SERVICE_FILES "/daniel/Desktop/evo_pipe/catkin_ws/src/canbus_interface/srv/CanFrameSrv.srv")
