baed099e7c727d95f6cea79acdc7f9e3d747343f
Daniel Frau Alfaro <98766327+DanielFrauAlfaro@users.noreply.github.com>
2023-10-18 13:21:50 +0200

    Gripper real controller with collision detection

--- catkin_ws/src/experiment_settings/scripts/main_gripper_controller_real.py
+++ catkin_ws/src/experiment_settings/scripts/main_gripper_controller_real.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import rospy
+import numpy as np
 import sys
-from std_msgs.msg import Float64, Float32
+from std_msgs.msg import Int32, Int16MultiArray
 from robotiq_3f_gripper_articulated_msgs.msg import Robotiq3FGripperRobotOutput, Robotiq3FGripperRobotInput
 
 # Messages
@@ -13,12 +14,27 @@ msg_3f.rGTO = 1
 # Publishers commands
 pub_cmd = []
 
-# States list
-pub_state = []
+
+# gDTA -> Contacto con el dedo A
+# gDTC -> Contacto con el dedo C
+# El valor de gDTB no indica el contacto, por lo que no se debería usar
 
 
-# Start the node
-start_node = False
+def input_cb(data):
+    global pub_cmd
+
+    msg = Int16MultiArray()
+
+    msg.data = np.array([0, 0])
+
+    if data.gDTA == 1:
+        msg.data[0] = 1
+    if data.gDTC == 1:
+        msg.data[1] == 1
+
+    pub_cmd[1].publish(msg)
+    
+
 
 # Position command callback
 def cmd_cb(data):
@@ -44,17 +60,14 @@ if __name__ == "__main__":
     # Node
     rospy.init_node("gripper_controller")
 
-    # --- Publishers ---
-    # Publisher of the gripper state
-    # pub_state.append(rospy.Publisher("/" + name + "/grip_state", Float32, queue_size=10))
-
     # --- Subscribers ---
     # Topic for the gripper commands
-    rospy.Subscriber("/aurova/grip_cmd", Float64, cmd_cb)
+    rospy.Subscriber("/aurova/grip_cmd", Int32, cmd_cb)
+    rospy.Subscriber("/Robotiq3FGripperRobotInput", Robotiq3FGripperRobotInput, input_cb)
 
     # --- Publishers ---
     pub_cmd.append(rospy.Publisher("/Robotiq3FGripperRobotOutput", Robotiq3FGripperRobotOutput, queue_size=10))
-    
+    pub_cmd.append(rospy.Publisher("/aurova/contacts_gripper", Int16MultiArray, queue_size=10))
 
     
     # Spin
